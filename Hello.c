19e0c6303ca67dbc471fb05467565c5b1c0816ce
codebyu <chttimtii@gmail.com>
2024-12-07 23:57:50 +0530

    C

--- Hello.c
+++ Hello.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 int main(){
-<<<<<<< HEAD
 printf("Hello World to all of you\n");
+printf("ThankYOU");
 return 0;
-=======
-    
-    printf("Hello World Is Hello World OK");
-    return 0;
->>>>>>> 831fffb (C)
 }

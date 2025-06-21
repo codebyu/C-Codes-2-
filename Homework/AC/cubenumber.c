eed76ef639e7814de414059f0a54e28c4f572e17
codebyu <chttimtii@gmail.com>
2025-06-21 14:02:32 +0530

    hi

--- Homework/AC/cubenumber.c
+++ Homework/AC/cubenumber.c
@@ -1,6 +1,7 @@
+//C Program to find out the cube of the no given by the user
 #include<stdio.h>
-int main(){
-        double a;
+int main(){                 //Main function
+double a;                   //Declaring double point no    
     printf("Enter the no to make its cube : ");
     scanf("%hf",&a);
     printf("The cube of the number is %hf",a*a*a);

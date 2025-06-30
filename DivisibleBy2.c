ed7a64a80cefd62c5e7668554c40195547118d5d
codebyu <chttimtii@gmail.com>
2025-07-01 00:36:21 +0530

    1 July

---
+++ DivisibleBy2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the no to check if it is divisible by 2 or not \n");
+    scanf("%d",&n);
+    printf("If Showing 1 the no is divisible by 2 & \nIf showing 0 it is not divisible \n%d",n%2==0);
+    
+}
---
+++ ValidationCheck.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int x; int y =x;  // This can be done
+    int a,b=a;          // This can be done
+    printf("%d\n",8^8);    //This not gives 8th power of 8 , it will perform xor operation 
+    char stars = '**';      //char stores only one character
+    printf("%c",stars);  
+}

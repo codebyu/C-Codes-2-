49c3981cda3022285070bc8f5c120d00e6b16163
codebyu <chttimtii@gmail.com>
2025-07-14 01:05:17 +0530

    14July

--- Homework/AC/NaturalNoCheck.c
+++ Homework/AC/NaturalNoCheck.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include <limits.h>
 int main(){
-    long n;
+    long long n;
     printf("Enter the no. to check if it is natural no. or not\n");
-    scanf("%ld",&n);
+    scanf("%lld",&n);
     if(n>0){
         printf("Natural No.\n");
     }
     else{
         printf("Not a Natural No.\n");
     }
-    printf("int min: %ld, int max: %ld\n", LONG_MIN, LONG_MAX);
+    printf("long min: %lld, long max: %lld\n", LONG_LONG_MIN, LONG_LONG_MAX);
+    // // usually in our windows int and long have same range but in 64 bit linux they usually long has high range 
+    // if we want such high range in windows usually we have to use long long
     return 0;
     
 

204867538b16e3997d40f65bc8275953c6de7632
codebyu <chttimtii@gmail.com>
2025-07-15 15:11:44 +0530

    15JULY

--- Homework/AC/Armstrong.c
+++ Homework/AC/Armstrong.c
@@ -1,29 +1,20 @@
 #include<stdio.h>
 int main(){
-    int n,c;
-    printf("Enter the no. to check it is an armstrong no. or not \n");
+    int n,arm=0,r,d;
+    printf("Enter the no to check\n");
     scanf("%d",&n);
-    if(n>=0&&n<=9){
-        c=1;
+    d=n;
+    while(d>0){
+        r=d%10;
+        arm= (r*r*r)+ arm;
+        d=d/10;
     }
-    else if(n>9&&n<100){
-        c=2;
-    }
-    else if(n>99&&n<1000){
-        c=3;
-    }
-    else if(n>999&&n<10000){
-        c=4;
-    }
+    if(n==arm){
+    printf("It is an Armstrong Number\n");
+    } 
     else{
-        printf("No. of digits in the given no. exceeded the maximum limit for this program ignore below result\n");
-        return 0;
+    printf("It is not an Armstrong Number\n");
     }
-    
-    if(c==1||n==153||n==370||n==371||n==407||n==1634||n==8208||n==9474){
-        printf("It is an armstrong number\n");
-    }
-    else{
-        printf("It is not an armstrong number\n");
-    }
+   
+
 }

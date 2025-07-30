84dd996fcce54c2838698eb7b22e98cb2d3c7b11
codebyu <chttimtii@gmail.com>
2025-07-30 18:01:56 +0530

    30 JULY

---
+++ LibraryFunSquare.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int n;
+
+int main(n){
+    int n;
+    printf("Enter the no. to find square\n");
+    scanf("%d",&n);
+    int c= main(2);
+    printf("%d",c);
+    return n*n;
+
+}

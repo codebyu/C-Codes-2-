db56aa48b285c4a1cf9b12a731b59df01f1a9833
codebyu <chttimtii@gmail.com>
2025-08-06 02:48:18 +0530

    6 Aug

---
+++ Homework/AC/PowFunction.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int pow(int a,int b){
+    int ans;
+    for(int i=1;i<=b;i++){
+        ans = a*a;
+        
+    }
+    return ans;
+}
+
+int main(){
+    printf("Enter the base no.\n");
+    int a;
+    scanf("%d",&a);
+    printf("What power of %d you want to find \n",a);
+    int b;
+    scanf("%d",&b);
+    printf("%d to the power %d is %d",a,b,pow(a,b));
+    return 0;
+}

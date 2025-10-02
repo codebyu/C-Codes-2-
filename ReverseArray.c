5f8a7b6c6d6f3c17f47ddbe458f3edb9e0df5b25
codebyu <chttimtii@gmail.com>
2025-10-03 02:33:34 +0530

    Update ReverseArray.c

--- ReverseArray.c
+++ ReverseArray.c
@@ -2,7 +2,7 @@
 int main(){
     int arr[]= {2,4,6,5,8,55,56,57,58};
      int size = (sizeof(arr)/4)-1;
-     printf("size : %d",size);
+     printf("size : %d\n",size);
     // 4-0 3-1 2-2 0
     int temp;
     for(int i=0;i<(size/2);i++){

073764f20fdfd2666a3aa37ea9a0396a78cfa45b
codebyu <chttimtii@gmail.com>
2025-10-14 01:40:09 +0530

    14oct

--- Homework/CWH/3dArrayAddress.c
+++ Homework/CWH/3dArrayAddress.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
     int arr[1][2][3] = {2,4,6,8,10,12};
-    for(int i=0;i<3;i++){
+    for(int i=0;i<2;i++){
         for(int j=0;j<3;j++)
-        printf("%u\t",&arr[0][i][j]);
+        printf("%p\t",(int)(&arr[0][i][j]));
     }
 }

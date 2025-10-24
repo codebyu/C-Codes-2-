9d58946fb21fffdd940e729ce7d784a5fec7004d
codebyu <chttimtii@gmail.com>
2025-10-24 13:33:00 +0530

    24oct

--- Homework/CWH/3dArrayAddress.c
+++ Homework/CWH/3dArrayAddress.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 int main(){
-    int arr[1][2][3] = {2,4,6,8,10,12};
+    int arr[2][2][3]={
+                        {{1,2,3},{4,5,8}},
+                        {{7,8,5},{6,8,10}}
+    };
     for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++)
-        printf("%p\t",(int)(&arr[0][i][j]));
+        for(int j=0;j<2;j++){
+            for(int k=0;k<3;k++){
+                printf("Address of %d is %u\t",arr[i][j][k],&arr[i][j][k]);
+            }
+            printf("\n");
+        }
     }
+
 }

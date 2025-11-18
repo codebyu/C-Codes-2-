1aa7cd8fae8feedfb7a737a0920943aa3c23165d
codebyu <chttimtii@gmail.com>
2025-11-19 02:28:29 +0530

    19 nov

---
+++ check.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("Enter 1: ");
+    char c1 ;
+    scanf("%c",&c1);
+    printf("Enter 2: ");
+    char c2 ;
+    scanf(" %c",&c2);
+    printf("%c and %c",c1,c2);
+}

f2138e5986586d1baedc61286e08fe7af53052bb
codebyu <chttimtii@gmail.com>
2025-11-29 17:28:06 +0530

    29nov

--- Homework/CWH/tempCodeRunnerFile.c
+++ Homework/CWH/tempCodeRunnerFile.c
@@ -1,1 +1,14 @@
-  
+#include<stdio.h>
+int main(){
+    char str[100];
+    printf("Enter string : ");
+    scanf("%c",&str[0]);
+    int i=1;
+    for(i;str[i-1]!='\n';i++){
+        scanf("%c",&str[i]);
+    }
+    str[i]='\0';
+    printf("Final : ");
+    puts(str);
+
+}
